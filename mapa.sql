4dec5d8f9680aee530195ed59f15ce2ac9d9cba2
DanielCondia <daniel.condia@gmail.com>
2024-10-29 23:23:45 -0500

    :sparkles: Se agregan entidades usuarios y coordenadas

--- mapa.sql
+++ mapa.sql
@@ -1,95 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 30-10-2024 a las 04:29:56
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+CREATE TABLE IF NOT EXISTS usuarios(
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `nombre_usuario` VARCHAR(20),
+    `numero_contacto` VARCHAR(20)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE IF NOT EXISTS coordenadas(
+    `longitud` VARCHAR(20),
+    `latitud` VARCHAR(20),
+    `id_usuario` INTEGER NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+);
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `mapa`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `coordenadas`
---
-
-CREATE TABLE `coordenadas` (
-  `id_coordenadas` int(11) NOT NULL,
-  `longitud` varchar(45) NOT NULL,
-  `latitud` varchar(45) NOT NULL,
-  `id_usuario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id_usuarios` int(11) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `numero_tel` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `coordenadas`
---
-ALTER TABLE `coordenadas`
-  ADD PRIMARY KEY (`id_coordenadas`),
-  ADD KEY `id_usuario` (`id_usuario`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id_usuarios`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `coordenadas`
---
-ALTER TABLE `coordenadas`
-  MODIFY `id_coordenadas` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id_usuarios` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `coordenadas`
---
-ALTER TABLE `coordenadas`
-  ADD CONSTRAINT `coordenadas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuarios`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE usuarios ADD INDEX (id);
